169ea2d48e3d0c1e05249204c218563acb23de72
mikezaby <mikezaby@gmail.com>
2013-09-01 17:55:03 +0300

    move problem1 functionality to separated function

--- problem1.rs
+++ problem1.rs
@@ -1,14 +1,9 @@
 fn main() {
-  let x = 5;
-  let y = 3;
-  let until = 999;
+  println(fmt!("%u", problem1(3, 5, 999)));
+}
+
+fn problem1(x: uint, y: uint, until: uint) -> uint {
   let mcm = x * y;
   let one_to_n = |n| { ((1 + n) * n) / 2 };
-
-  let x_count = until / x;
-  let y_count = until / y;
-  let mcm_count = until / mcm;
-
-  let sum = y * one_to_n(y_count) + x * one_to_n(x_count) - mcm * one_to_n(mcm_count);
-  println(fmt!("%d", sum));
+  y * one_to_n(until / y) + x * one_to_n(until / x) - mcm * one_to_n(until / mcm)
 }
